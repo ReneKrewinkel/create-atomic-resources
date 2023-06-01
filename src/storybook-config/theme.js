c2a04c355b4963edb0bf49854a3852fd07b926c9
renekrewinkel <rene@studiokrijst.com>
2023-06-01 09:12:06 +0200

    2.1.2

--- src/storybook-config/theme.js
+++ src/storybook-config/theme.js
@@ -2,7 +2,7 @@ import { create } from '@storybook/theming/create'
 
 export default create({
   base: 'dark',
-  brandTitle: 'Blue Sky UCS',
+  brandTitle: 'Brand Title',
   brandUrl: 'https://url',
   brandImage: '/logo-on-url.svg',
   brandTarget: '_self',
