797e455e6ba234aaf0e8bca2bb7d03ccbc357bb2
renekrewinkel <rene@krewinkel.nl>
2024-10-08 16:10:21 +0200

    2.1.3

--- bin/index.js
+++ bin/index.js
@@ -1,67 +1,67 @@
 #! /usr/bin/env node
 
-import chalk from 'chalk'
-import fs from 'fs-extra'
-import * as url from 'url'
+import chalk from "chalk";
+import fs from "fs-extra";
+import * as url from "url";
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url)).slice(0, -1)
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url)).slice(0, -1);
 
-const appPackage = `${__dirname}/../package.json`
-const templatePath = `${__dirname}/src`.replace('/bin', '')
-
+const appPackage = `${__dirname}/../package.json`;
+const templatePath = `${__dirname}/src`.replace("/bin", "");
 
 const convertToPascalCase = (s) => {
   const r = s.replace(
     /\w+/g,
-    (word) => word[0].toUpperCase() + word.slice(1).toLowerCase()
-  )
-  return r.split(' ').join('')
-}
+    (word) => word[0].toUpperCase() + word.slice(1).toLowerCase(),
+  );
+  return r.split(" ").join("");
+};
 
 const readConfig = () => {
-  const cfg = JSON.parse(fs.readFileSync(appPackage, 'utf8'))
+  const cfg = JSON.parse(fs.readFileSync(appPackage, "utf8"));
   return [
     cfg.name,
-    convertToPascalCase(cfg.name.replace('-', ' ')),
+    convertToPascalCase(cfg.name.replace("-", " ")),
     cfg.version,
     cfg.author,
-    cfg.files.src
-  ]
-}
+    cfg.files.src,
+  ];
+};
 
-const [appName, appBanner, appVersion, appAuthor, templateRep] = readConfig()
+const [appName, appBanner, appVersion, appAuthor, templateRep] = readConfig();
 
 const showCopyright = () => {
-  console.log(`\n💥 ${appName} v${appVersion} © ${appAuthor}`)
-}
+  console.log(`\n💥 ${appName} v${appVersion} © ${appAuthor}`);
+};
 
 const error = (msg) => {
-  console.log(chalk.red(`💀 ${msg}`))
-  process.exit(1)
-}
+  console.log(chalk.red(`💀 ${msg}`));
+  process.exit(1);
+};
 
 const success = (msg) => {
-  console.log(chalk.greenBright(`🤙 ${msg}`))
-}
+  console.log(chalk.greenBright(`🤙 ${msg}`));
+};
 
 const usage = () => {
-  const msg =`USAGE: npx ${appName} <destination dir>`
-   console.log(chalk.greenBright(`🤙 ${msg}`))
-   process.exit(2)
-}
+  const msg = `USAGE: npx ${appName} <destination dir>`;
+  console.log(chalk.greenBright(`🤙 ${msg}`));
+  process.exit(2);
+};
 
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 if (args.length < 1) {
-  usage()
+  usage();
 } else {
-  const currentDir = `./${ args[0].replace('./', '') }`
+  const currentDir = `./${args[0].replace("./", "")}`;
   try {
-    fs.ensureDir(currentDir)
-    fs.copySync(templatePath, currentDir, { overwrite: true })
-    success(`Resources (resources, documentation, storybook-config) installed in ${currentDir}`)
-    showCopyright()
+    fs.ensureDir(currentDir);
+    fs.copySync(templatePath, currentDir, { overwrite: true });
+    success(
+      `Resources (token, fonts, scss) installed in ${currentDir}/resources`,
+    );
+    showCopyright();
   } catch (err) {
-    error(err.message)
+    error(err.message);
   }
 }
-
